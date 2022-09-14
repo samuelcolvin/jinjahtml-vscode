02ac24808bebebc719b07d00d656b0eeefb2cbb2
Matthew McCune <matt@mattrick.org>
2022-09-13 21:09:07 -0700

    Activate vscode.html-language-features when Jinja HTML is opened

---
+++ src/extension.ts
@@ -0,0 +1,18 @@
+import * as vscode from 'vscode'
+
+// This hack is necessary to ensure that the vscode.html-language-features extension is activated in order to enable
+// htmlLanguageParticipants support. Please see https://github.com/microsoft/vscode/issues/160585 for more info.
+export async function activate() {
+  const htmlExtension = vscode.extensions.getExtension('vscode.html-language-features')
+
+  if (!htmlExtension) {
+    const output = vscode.window.createOutputChannel('Jinja')
+
+    output.appendLine(
+      'Warning: Could not find vscode.html-language-features. HTML Language Participants support will be disabled.',
+    )
+    return
+  }
+
+  await htmlExtension?.activate()
+}
